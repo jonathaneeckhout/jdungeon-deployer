d563ab7b314ed08d7a98220299f51877e585ced5
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2023-07-28 20:28:13 +0200

    save pos and experience

---
+++ jdungeon-database/migration/V1__Create_jdungeon_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE levels (
+    id SERIAL PRIMARY KEY,
+    level VARCHAR(255) NOT NULL UNIQUE,
+    key VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    port INTEGER NOT NULL
+);
+
+CREATE TABLE characters (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    player VARCHAR(255) NOT NULL,
+    level VARCHAR(255) NOT NULL,
+    pos_x REAL,
+    pos_y REAL,
+    exp INTEGER DEFAULT 0,
+    exp_level INTEGER DEFAULT 1
+);
