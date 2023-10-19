31061cd00c453743cf53e179882a1a27c929f5a1
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2023-10-19 16:38:19 +0200

    deploy new jdungeon

--- jdungeon-database/migration/V1__Create_jdungeon_tables.sql
+++ jdungeon-database/migration/V1__Create_jdungeon_tables.sql
@@ -1,26 +1,6 @@
-CREATE TABLE players (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE levels (
-    id SERIAL PRIMARY KEY,
-    level VARCHAR(255) NOT NULL UNIQUE,
-    key VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    port INTEGER NOT NULL
-);
-
-CREATE TABLE characters (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    player VARCHAR(255) NOT NULL,
-    level VARCHAR(255) NOT NULL,
-    pos_x REAL,
-    pos_y REAL,
-    stats json DEFAULT '{"hp":10, "level":1, "exp":0}',
-    inventory json DEFAULT '{"gold":0, "items": []}',
-    equipment json DEFAULT '{"equipment":{}}'
+    password VARCHAR(255) NOT NULL,
+    data json DEFAULT '{}'
 );
